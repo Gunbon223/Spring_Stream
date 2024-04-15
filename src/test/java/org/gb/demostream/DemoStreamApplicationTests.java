fb9cb8a6825f00e483fb1fcaebb56ab42d079cc1
Gunbon223 <gunbon610@gmail.com>
2024-04-15 21:23:41 +0700

    Initial commit

---
+++ src/test/java/org/gb/demostream/DemoStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gb.demostream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
